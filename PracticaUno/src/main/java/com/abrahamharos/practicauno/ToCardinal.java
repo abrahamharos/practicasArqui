e6c2a2e66bba542b7cd5439c72a6585d8ba4e271
Santiago Díaz Guevara <santidiazg@hotmail.es>
2022-03-16 20:01:35 -0600

    Fix issue with 'veintiocho' being written as two words

--- PracticaUno/src/main/java/com/abrahamharos/practicauno/ToCardinal.java
+++ PracticaUno/src/main/java/com/abrahamharos/practicauno/ToCardinal.java
@@ -34,34 +34,62 @@ public class ToCardinal implements To {
 
     @Override
     public String to(int i) {
-        // Early exit if value exists in dictionary.
-        if (dictionary.has(Integer.toString(i))) {
-            return this.get(Integer.toString(i), GrammarType.SINGULAR);
+        if (i < 0) {
+            return "Error: integer must be positive";
         }
-
-        int current, partialSum = 0, order = 0;
-        List<String> result = new ArrayList<>();
-
+        
+        if (i == 0) {
+            return "cero";
+        }
+        
+        // `result` is used to build up the numerical text piece by piece.
+        String result = "";
+        
+        // `partialText` is the result of lookups in the dictionary.
+        String partialText;
+        
         while (i > 0) {
-            current = (i % 10) * (int) Math.pow(10, order);
-            partialSum += current;
-
-            if (current != 0) {
-                result.add(this.get(Integer.toString(current), partialSum > 0 ? GrammarType.PLURAL : GrammarType.SINGULAR));
+            // Reset `partialText`.
+            partialText = "";
+            
+            // Return early if `i` is found on the dictionary.
+            //
+            // If this conditional is skipped, then we know for sure
+            // that the highest order digit in `i` is not followed
+            // by zeros in lower order digits, since that would appear
+            // in the dictionary.
+            if (this.dictionary.has(Integer.toString(i))) {
+                // Get `i`'s text value.
+                partialText = this.get(Integer.toString(i), GrammarType.SINGULAR);
+                
+                // Append `partialText` to `result`.
+                result = result.concat(" " + partialText);
+                
+                return result.trim();
             }
+            
+            // Take highest order digit value, convert it to text, concatenate
+            // the text to `result`, and continue with the value of
+            // lower order digits.
+            
+            // The `order` of the number, AKA the number of digits - 1.
+            int order = (int) Math.log10(i);
+            
+            // `top` contains the value of the highest order digit.
+            // For example, if `i` is 9876, then `top` would be 9000.
+            int top = (i / (int) Math.pow(10, order)) * (int) Math.pow(10, order);
+            
+            // `partialText` is the result for `top`'s lookup in the dictionary.
+            partialText = this.get(Integer.toString(top), GrammarType.PLURAL);
 
-            String partialSumString = Integer.toString(partialSum);
-            if (dictionary.has(partialSumString)) {
-                result = result.subList(0, Math.max(0, result.size() - partialSumString.length()));
-                result.add(this.get(partialSumString, GrammarType.SINGULAR));
-            }
-
-            i /= 10;
-            ++order;
+            // Concatenate 
+            result = result.concat(" " + partialText);
+            
+            // Update `i` to contain the leftover value between `i` and `top`.
+            i = i - top;
         }
-
-        Collections.reverse(result);
-        return String.join(" ", result);
+        
+        return result.trim();
     }
 
     private String get(String integer, GrammarType grammarType) {
